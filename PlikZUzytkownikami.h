dd480948809757b927a3682d839f0a950334c18e
Przemek Madej <przemyslaw.madej.programista@gmail.com>
2023-03-06 10:12:15 +0100

    featured refactored code by adding inheritance

--- PlikZUzytkownikami.h
+++ PlikZUzytkownikami.h
@@ -6,22 +6,22 @@
 #include <vector>
 #include <cstdlib>
 
+#include "PlikTekstowy.h"
 #include "Uzytkownik.h"
 #include "MetodyPomocnicze.h"
 
+
 using namespace std;
 
-class PlikZUzytkownikami
+class PlikZUzytkownikami :public PlikTekstowy
 {
-    const string nazwaPlikuZUzytkownikami;
 
-    bool czyPlikJestPusty(fstream &plikTekstowy);
     string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
     Uzytkownik pobierzDaneUzytkownika(string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
 
 public:
 
-    PlikZUzytkownikami(string NAZWAPLIKUZUZYTKOWNIKAMI) : nazwaPlikuZUzytkownikami(NAZWAPLIKUZUZYTKOWNIKAMI){};
+    PlikZUzytkownikami(string nazwaPliku) : PlikTekstowy(nazwaPliku) {};
     void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
     vector <Uzytkownik> wczytajUzytkownikowZPliku();
     void zapiszWszystkichUzytkownikowDoPliku(vector <Uzytkownik> &uzytkownicy);
